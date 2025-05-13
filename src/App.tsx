fdd5c02030af41b01d48ebc3dcbe12c1225efa25
streetlightkids <ltj_hq@hotmail.com>
2025-05-12 20:28:35 -0700

    Markup for the rest of the form

--- src/App.tsx
+++ src/App.tsx
@@ -152,78 +152,229 @@ function App() {
         </label>
       </fieldset>
       <fieldset>
-        Income 1a Total amount from Form(s) W-2, box 1 (see instructions){" "}
-        <input type="number" /> b Household employee wages not reported on
-        Form(s) W-2
-        <input type="number" /> c Tip income not reported on line 1a (see
-        instructions) <input type="number" /> d Medicaid waiver payments not
-        reported on Form(s) W-2 (see instructions)
-        <input type="number" /> e Taxable dependent care benefits from ⚠️ Form
-        2441, line 26 <input type="number" /> f Employer-provided adoption
-        benefits from ⚠️ Form 8839, line 29 <input type="number" /> g Wages from
-        ⚠️ Form 8919, line 6 <input type="number" /> h Other earned income (see
-        instructions) <input type="number" /> i Nontaxable combat pay election
-        (see instructions)
-        <input type="number" /> z Add lines 1a through 1h{" "}
-        <input type="number" /> 2a Tax-exempt interest <input type="number" /> b
-        Taxable interest <input type="number" /> 3a Qualified dividends{" "}
-        <input type="number" /> b Ordinary dividends <input type="number" /> 4a
-        IRA distributions <input type="number" /> b Taxable amount{" "}
-        <input type="number" /> 5a Pensions and annuities{" "}
-        <input type="number" /> b Taxable amount <input type="number" /> 6a
-        Social security benefits <input type="number" /> b Taxable amount c If
-        you elect to use the lump-sum election method, check here{" "}
-        <input type="checkbox" /> 7 Capital gain or (loss). Attach Schedule D if
-        required. <input type="number" /> b If not required check here{" "}
-        <input type="checkbox" /> 8 Additional income from ⚠️ Schedule 1, line
-        10 <input type="number" /> 9 Add lines 1z, 2b, 3b, 4b, 5b, 6b, 7 and 8.
-        This is your **total income** <input type="number" />
-        10 Adjustments from ⚠️ Schedule 1, line 26 <input type="number" /> 11
-        Subtract line 10 from line 9. This is your **adjusted gross income**{" "}
-        <input type="number" /> 12 Standard deduction or itemized deductions
-        (from ⚠️ Schedule A) <input type="number" /> 13 Qualified business
-        income deduction from ⚠️ Form 8995 or ⚠️ Form 8995-A{" "}
-        <input type="number" /> 14 Add lines 12 and 13 <input type="number" />{" "}
-        15 Subtract line 14 from line 11. If zero or less, enter "-0-". This is
-        your **taxable income** <input type="number" /> 16 **Tax** (see
-        instructions).
-        <input type="number" />{" "}
+        <label htmlFor="income-total-amount">
+          Income 1a Total amount from Form(s) W-2, box 1 (see instructions){" "}
+        </label>
+        <input type="number" id="income-total-amount" />{" "}
+        <label htmlFor="employee-wages">
+          b Household employee wages not reported on Form(s) W-2
+        </label>
+        <input type="number" id="employee-wages" />{" "}
+        <label htmlFor="tip-income">
+          c Tip income not reported on line 1a (see instructions)
+        </label>{" "}
+        <input type="number" id="tip-income" />{" "}
+        <label htmlFor="medicaid-waiver-payments">
+          d Medicaid waiver payments not reported on Form(s) W-2 (see
+          instructions)
+        </label>
+        <input type="number" id="medicaid-waiver-payments" />{" "}
+        <label htmlFor="taxable-dependent-benefits">
+          e Taxable dependent care benefits from ⚠️ Form 2441, line 26{" "}
+        </label>
+        <input type="number" id="taxable-dependent-benefits" />{" "}
+        <label htmlFor="employer-adoption-benefits">
+          f Employer-provided adoption benefits from ⚠️ Form 8839, line 29
+        </label>{" "}
+        <input type="number" id="employer-adoption-benefits" />{" "}
+        <label htmlFor="wages-from-form">
+          g Wages from ⚠️ Form 8919, line 6{" "}
+        </label>
+        <input type="number" id="wages-from-form" />{" "}
+        <label htmlFor="other-income">
+          h Other earned income (see instructions)
+        </label>{" "}
+        <input type="number" id="other-income" />{" "}
+        <label htmlFor="combat-payments">
+          i Nontaxable combat pay election (see instructions)
+        </label>
+        <input type="number" id="combat-payments" />{" "}
+        <label htmlFor="add-lines">z Add lines 1a through 1h </label>
+        <input type="number" id="add-lines" />{" "}
+        <label htmlFor="exempt-interest">2a Tax-exempt interest </label>
+        <input type="number" id="exempt-interest" />{" "}
+        <label htmlFor="taxable-interest">b Taxable interest </label>
+        <input type="number" id="taxable-interest" />{" "}
+        <label htmlFor="qualified-dividends">3a Qualified dividends </label>
+        <input type="number" id="qualified-dividends" />{" "}
+        <label htmlFor="ordinary-dividends">b Ordinary dividends </label>
+        <input type="number" id="ordinary-dividends" />{" "}
+        <label htmlFor="ira-dist">4a IRA distributions </label>
+        <input type="number" id="ira-dist" />{" "}
+        <label htmlFor="taxable-amount-1">b Taxable amount </label>
+        <input type="number" id="taxable-amount-1" />{" "}
+        <label htmlFor="pensions-annuities">5a Pensions and annuities </label>
+        <input type="number" id="pensions-annuities" />{" "}
+        <label htmlFor="taxable-amount-2"></label>b Taxable amount{" "}
+        <input type="number" id="taxable-amount-2" />{" "}
+        <label htmlFor="ss-benefits">6a Social security benefits </label>
+        <input type="number" id="ss-benefits" />{" "}
+        <label htmlFor="taxable-amount-3">b Taxable amount</label>{" "}
+        <input type="number" id="taxable-amount-3" />{" "}
+        <label htmlFor="lump-sum-election">
+          c If you elect to use the lump-sum election method, check here{" "}
+        </label>
+        <input type="checkbox" id="lump-sum-election" />{" "}
+        <label htmlFor="capital-gain-loss-req">
+          7 Capital gain or (loss). Attach Schedule D if required.
+        </label>{" "}
+        <input type="number" id="capital-gain-loss-req" />{" "}
+        <label htmlFor="capital-gain-loss-non-req">
+          b If not required check here{" "}
+        </label>
+        <input type="checkbox" id="capital-gain-loss-non-req" />{" "}
+        <label htmlFor="additional-income">
+          8 Additional income from ⚠️ Schedule 1, line 10{" "}
+        </label>
+        <input type="number" id="additional-income" />{" "}
+        <label htmlFor="total-income">
+          9 Add lines 1z, 2b, 3b, 4b, 5b, 6b, 7 and 8. This is your **total
+          income**{" "}
+        </label>
+        <input type="number" id="total-income" />
+        <label htmlFor="adjustments">
+          10 Adjustments from ⚠️ Schedule 1, line 26{" "}
+        </label>
+        <input type="number" id="adjustments" />{" "}
+        <label htmlFor="adjusted-gross">
+          11 Subtract line 10 from line 9. This is your **adjusted gross
+          income**{" "}
+        </label>
+        <input type="number" id="adjusted-gross" />{" "}
+        <label htmlFor="standard-deduction">
+          12 Standard deduction or itemized deductions (from ⚠️ Schedule A){" "}
+        </label>
+        <input type="number" id="standard-deduction" />{" "}
+        <label htmlFor="business-income">
+          13 Qualified business income deduction from ⚠️ Form 8995 or ⚠️ Form
+          8995-A{" "}
+        </label>
+        <input type="number" id="business-income" />{" "}
+        <label htmlFor="add-12-and-13">14 Add lines 12 and 13 </label>
+        <input type="number" id="add-12-and-13" />{" "}
+        <label htmlFor="taxable-income"></label>15 Subtract line 14 from line
+        11. If zero or less, enter "-0-". This is your **taxable income**{" "}
+        <input type="number" id="taxable-income" />{" "}
+        <label htmlFor="tax">16 **Tax** (see instructions).</label>
+        <input type="number" id="tax" />
+        Check if any from Form(s): <input type="checkbox" id="8814" />{" "}
+        <label htmlFor="8814">8814</label> <input type="checkbox" id="4972" />{" "}
+        <label htmlFor="4972"> 4972</label>{" "}
+        <input type="checkbox" id="manual-form-number-checkbox" />{" "}
+        <input type="text" id="manual-form-number-input" />
+        <label htmlFor="amount-from-schedule-2">
+          17 Amount from ⚠️ Schedule 2, line 3{" "}
+        </label>
+        <input type="number" id="amount-from-schedule-2" />{" "}
+        <label htmlFor="add-16-and-18"></label>18 Add lines 16 and 17{" "}
+        <input type="number" id="add-16-and-18" />{" "}
+        <label htmlFor="child-tax-credit">
+          19 Child tax credit or credit for other dependents from ⚠️ Schedule
+          8812
+        </label>{" "}
+        <input type="number" id="child-tax-credit" />{" "}
+        <label htmlFor="amount-from-schedule-3">
+          20 Amount from Schedule 3, line 8
+        </label>{" "}
+        <input type="number" id="amount-from-schedule-3" />{" "}
+        <label htmlFor="add-19-and-20">21 Add lines 19 and 20 </label>
+        <input type="number" id="add-19-and-20" />{" "}
+        <label htmlFor="subtract-21-from-18">
+          22 Subtract line 21 from line 18. If zero or less, enter "-0-"{" "}
+        </label>
+        <input type="number" id="subtract-21-from-18" />{" "}
+        <label htmlFor="other-taxes">
+          23 Other taxes, including self-employment tax, from ⚠️ Schedule 2,
+          line 21{" "}
+        </label>
+        <input type="number" id="other-taxes" />{" "}
+        <label htmlFor="total-taxes">
+          24 Add lines 22 and 23. This is your **total tax**{" "}
+        </label>
+        <input type="number" id="total-taxes" />{" "}
       </fieldset>
-      Check if any from Form(s): <input type="checkbox" /> 8814{" "}
-      <input type="checkbox" /> 4972 <input type="checkbox" />{" "}
-      <input type="text" />
-      17 Amount from ⚠️ Schedule 2, line 3 <input type="number" /> 18 Add lines
-      16 and 17 <input type="number" /> 19 Child tax credit or credit for other
-      dependents from ⚠️ Schedule 8812 <input type="number" /> 20 Amount from
-      Schedule 3, line 8 <input type="number" /> 21 Add lines 19 and 20{" "}
-      <input type="number" /> 22 Subtract line 21 from line 18. If zero or less,
-      enter "-0-" <input type="number" /> 23 Other taxes, including
-      self-employment tax, from ⚠️ Schedule 2, line 21 <input type="number" />{" "}
-      24 Add lines 22 and 23. This is your **total tax** <input type="number" />{" "}
-      Payments 25 Federal income tax withheld from: a Form(s) W-2{" "}
-      <input type="number" /> b Form(s) 1099 <input type="number" /> c Other
-      forms (see instructions) <input type="number" /> d Add lines 25a through
-      25c <input type="number" /> 26 2024 estimated tax payments and amount
-      applied from 2023 return <input type="number" /> 27 Earned income credit
-      (EIC) <input type="number" /> 28 Additional child tax credit from ⚠️
-      Schedule 8812 <input type="number" /> 29 American opportunity credit from
-      Form 8863, line 8 <input type="number" /> 30 Reserved for future use ---
-      31 Amount from ⚠️ Schedule 3, line 15 <input type="number" /> 32 Add lines
-      27, 28, 29, 31. These are your **total other payments and refundable
-      credits**
-      <input type="number" /> 33 Add lines 25d, 26, and 32. These are your
-      **total payments** <input type="number" />
-      Refund 34 If line 33 is more than line 24, subtract line 22 from line 33.
-      This is the amount you **overpaid** <input type="number" /> 35a Amount of
-      line 34 you want refunded to you. <input type="number" />{" "}
-      <input type="checkbox" /> If ⚠️ Form 8888 is attached check here b Routing
-      number <input type="text" /> c Type: <input type="checkbox" /> Checking{" "}
-      <input type="checkbox" />
-      Savings d Account number <input type="text" /> 36 Amount of line 34 you
-      want **applied to your 2025 estimated tax** <input type="number" /> Amount
-      You Owe 37 Subtract line 33 from line 24. This is the **amount you owe**.{" "}
-      <input type="number" />
-      38 Estimated tax penalty (see instructions) <input type="number" />
+      <fieldset>
+        Payments{" "}
+        <label htmlFor="tax-witheld">
+          25 Federal income tax withheld from: a Form(s) W-2
+        </label>
+        <input type="number" id="tax-witheld" />{" "}
+        <label htmlFor="1099-tax-witheld">b Form(s) 1099 </label>
+        <input type="number" id="1099-tax-witheld" />{" "}
+        <label htmlFor="other-forms-tax-witheld">
+          c Other forms (see instructions){" "}
+        </label>
+        <input type="number" id="other-forms-tax-witheld" />{" "}
+        <label htmlFor="add-25a-thru-25c">d Add lines 25a through 25c </label>
+        <input type="number" id="add-25a-thru-25c" />{" "}
+        <label htmlFor="tax-payments-and-2023-return">
+          26 2024 estimated tax payments and amount applied from 2023 return
+        </label>{" "}
+        <input type="number" id="tax-payments-and-2023-return" />{" "}
+        <label htmlFor="">27 Earned income credit (EIC) </label>
+        <input type="number" id="earned-credit" />{" "}
+        <label htmlFor="additional-child-tax-credit">
+          28 Additional child tax credit from ⚠️ Schedule 8812{" "}
+        </label>
+        <input type="number" id="additional-child-tax-credit" />{" "}
+        <label htmlFor="op-credit">
+          29 American opportunity credit from Form 8863, line 8{" "}
+        </label>
+        <input type="number" id="op-credit" />{" "}
+        <label htmlFor="schedule-3-amount">
+          30 Reserved for future use --- 31 Amount from ⚠️ Schedule 3, line 15{" "}
+        </label>
+        <input type="number" id="schedule-3-amount" />
+        <label htmlFor="total-other-payments-and-refund-credits">
+          32 Add lines 27, 28, 29, 31. These are your **total other payments and
+          refundable credits**
+        </label>
+        <input type="number" id="total-other-payments-and-refund-credits" />
+        <label htmlFor="total-payments">
+          33 Add lines 25d, 26, and 32. These are your **total payments**
+        </label>{" "}
+        <input type="number" id="total-payments" />
+      </fieldset>
+      <fieldset>
+        Refund
+        <label htmlFor="amount-overpaid">
+          34 If line 33 is more than line 24, subtract line 22 from line 33.
+          This is the amount you **overpaid**{" "}
+        </label>
+        <input type="number" id="amount-overpaid" />
+        <label htmlFor="line-34-refund-amount">
+          35a Amount of line 34 you want refunded to you.
+        </label>
+        <input type="number" id="line-34-refund-amount" />{" "}
+        <input type="checkbox" id="8888-attached" />
+        <label htmlFor="8888-attached">
+          If ⚠️ Form 8888 is attached check here
+        </label>
+        <label htmlFor="routing-number">b Routing number</label>
+        <input type="text" id="routing-number" />
+        c Type:
+        <input type="checkbox" id="checking" />
+        <label htmlFor="checking">Checking</label>
+        <input type="checkbox" id="savings-account" />
+        <label htmlFor="savings-account">Savings</label>
+        <label htmlFor="savings-number"> d Account number </label>
+        <input type="text" id="savings-number" />{" "}
+        <label htmlFor="2025-applied-taxes">
+          36 Amount of line 34 you want **applied to your 2025 estimated tax**{" "}
+        </label>
+        <input type="number" id="2025-applied-taxes" />{" "}
+      </fieldset>
+      <fieldset>
+        <label htmlFor="amount-owed">
+          Amount You Owe 37 Subtract line 33 from line 24. This is the **amount
+          you owe**.
+        </label>{" "}
+        <input type="number" id="amount-owed" />
+        <label htmlFor="tax-penalty">
+          38 Estimated tax penalty (see instructions)
+        </label>{" "}
+        <input type="number" id="tax-penalty" />
+      </fieldset>
     </form>
   );
 }
